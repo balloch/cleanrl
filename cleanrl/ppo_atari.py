89b61c4ba16e3c67b9e05a4e554e560befee3188
balloch <balloch@gatech.edu>
2023-12-12 16:20:01 -0500

    beginning gymnasium update of cleanrl

--- cleanrl/ppo_atari.py
+++ cleanrl/ppo_atari.py
@@ -5,7 +5,7 @@ import random
 import time
 from distutils.util import strtobool
 
-import gym
+import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
@@ -100,7 +100,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
         env = gym.wrappers.ResizeObservation(env, (84, 84))
         env = gym.wrappers.GrayScaleObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
-        env.seed(seed)
+        env.seed(seed)  # TODO Deprecated
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
         return env
@@ -192,7 +192,8 @@ if __name__ == "__main__":
     # TRY NOT TO MODIFY: start the game
     global_step = 0
     start_time = time.time()
-    next_obs = torch.Tensor(envs.reset()).to(device)
+    init_observations, init_infos = envs.reset()
+    next_obs = torch.Tensor(init_observations).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
 
@@ -216,16 +217,20 @@ if __name__ == "__main__":
             logprobs[step] = logprob
 
             # TRY NOT TO MODIFY: execute the game and log data.
-            next_obs, reward, done, info = envs.step(action.cpu().numpy())
+            next_obs, reward, terminated, truncated, info = envs.step(action.cpu().numpy()) # Modified for gymnasium by balloch
+            done = np.logical_or(terminated, truncated) # Modified for gymnasium by balloch
             rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
 
-            for item in info:
-                if "episode" in item.keys():
-                    print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
-                    writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
-                    writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
-                    break
+            # Modified for gymnasium by balloch
+            if 'final_info' in info:
+                for item in info['final_info']:
+                    if isinstance(item, dict) and "episode" in item.keys():
+                        # for item in info:
+                        print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+                        writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+                        writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+                        break
 
         # bootstrap value if not done
         with torch.no_grad():
