5268f592d6d4821446ba3a901791b4d866049373
balloch <jon.balloch@gmail.com>
2024-04-17 16:31:44 +0000

    original changes from https://github.com/dosssman/cleanrl/tree/mbrl-dreamer

--- tests/test_atari.py
+++ tests/test_atari.py
@@ -15,3 +15,32 @@ def test_ppo_lstm():
         shell=True,
         check=True,
     )
+
+def test_dreamer_disc_latents():
+    subprocess.run(
+        "python cleanrl/dreamer_atari.py --env-id BreakoutNoFrameskip-v4 --buffer-prefill 400 --total-timesteps 410 --buffer-size 2000 --batch-size 4 --batch-length 50 --train-every 1 --viz-videos-every 5",
+        shell=True,
+        check=True,
+    )
+
+def test_dreamer_disc_latents_num_envs_4():
+    subprocess.run(
+        "python cleanrl/dreamer_atari.py --env-id BreakoutNoFrameskip-v4 --buffer-prefill 400 --total-timesteps 410 --buffer-size 2000 --batch-size 4 --batch-length 50 --train-every 1 --viz-videos-every 5 --num-envs 4",
+        shell=True,
+        check=True,
+
+    )
+
+def test_dreamer_cont_latents():
+    subprocess.run(
+        "python cleanrl/dreamer_atari.py --env-id BreakoutNoFrameskip-v4 --buffer-prefill 400 --total-timesteps 410 --buffer-size 2000 --batch-size 4 --batch-length 50 --train-every 1 --viz-videos-every 5 --rssm-discrete 0",
+        shell=True,
+        check=True,
+    )
+
+def test_dreamer_disc_latents_nodiscpred():
+    subprocess.run(
+        "python cleanrl/dreamer_atari.py --env-id BreakoutNoFrameskip-v4 --buffer-prefill 400 --total-timesteps 410 --buffer-size 2000 --batch-size 4 --batch-length 50 --train-every 1 --viz-videos-every 5 --disc-pred-hid-size 0",
+        shell=True,
+        check=True,
+    )
